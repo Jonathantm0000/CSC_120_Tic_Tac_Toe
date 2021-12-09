920c12f0ed71f03e663a6d89643752d8716d6de9
Jonathan Merritt <jmerritt4@my.waketech.edu>
2021-12-09 17:21:54 -0500

    Added alternating turns and let players take tiles

--- board.py
+++ board.py
@@ -1,4 +1,36 @@
 rows = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
+turn = 'o'
 
-for x in range(len(rows)):
-    print(rows[x])
+
+def printboard():
+    for x in range(len(rows)):
+        print(rows[x])
+
+
+def placexo(move1, move2, xoturn):
+            if rows[move1-1][move2-1] == '-':
+                rows[move1-1][move2-1] = xoturn
+            else:
+                print("that tile is taken")
+
+
+win = False
+while not win:
+    printboard()
+    if turn == 'x':
+        turn = 'o'
+    else:
+        turn = 'x'
+
+    print("It is", turn, "turn")
+    movec = int(input("Enter the column you want"))
+    mover = int(input("Enter the row you want"))
+    if movec < 1 | movec > 4:
+        print("Enter a column from 1-3")
+        movec = int(input("Enter the column you want"))
+
+    if mover < 1 | mover > 4:
+        print("Enter a column from 1-3")
+        mover = int(input("Enter the row you want"))
+
+    placexo(movec, mover, turn)
