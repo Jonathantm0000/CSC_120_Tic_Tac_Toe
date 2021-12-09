0cd3931356ad666069e035725eefaf7c62a5147e
Jonathan Merritt <jmerritt4@my.waketech.edu>
2021-12-09 18:28:49 -0500

    Added a win condition

--- board.py
+++ board.py
@@ -14,8 +14,28 @@ def placexo(move1, move2, xoturn):
                 print("that tile is taken")
 
 
+def wincheck(currentplayer):
+    if rows[0][0] == rows[0][1] == rows[0][2]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[1][0] == rows[1][1] == rows[1][2]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[2][0] == rows[2][1] == rows[2][2]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[0][0] == rows[1][0] == rows[2][0]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[0][1] == rows[1][1] == rows[2][1]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[0][2] == rows[1][2] == rows[2][2]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[0][0] == rows[1][1] == rows[2][2]:
+        print("Game over ", currentplayer, "wins")
+    elif rows[2][0] == rows[1][1] == rows[0][2]:
+        print("Game over ", currentplayer, "wins")
+
+
 win = False
 while not win:
+    turncount = 0
     printboard()
     if turn == 'x':
         turn = 'o'
@@ -34,3 +54,8 @@ while not win:
         mover = int(input("Enter the row you want"))
 
     placexo(movec, mover, turn)
+    wincheck(turn)
+    turncount = turncount + 1
+    if turncount == 9:
+        win = True
+        print("This game is a draw")
